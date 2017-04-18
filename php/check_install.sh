26918142bd444215081386f1d5f0b8614f2aa38c
Philip Strutzenberger <philip.strutzenberger@eitea.at>
2017-04-18 15:51:51 +0200

    Update check_install.sh

--- php/check_install.sh
+++ php/check_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 file=/var/www/html/home.php
 if [ -e "$file" ]; then
-    echo "File does not exist"
+    echo "File does exist"
 else 
     git clone --progress https://github.com/eitea/T-Time.git /var/www/html/
 fi 

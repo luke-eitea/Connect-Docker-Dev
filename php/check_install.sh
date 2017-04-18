5e70ab23a1ab379f5d2df0597f7232ee047b9f52
Philip Strutzenberger <philip.strutzenberger@eitea.at>
2017-04-18 16:16:22 +0200

    Update check_install.sh

--- php/check_install.sh
+++ php/check_install.sh
@@ -4,5 +4,9 @@ if [ -e "$file" ]; then
     echo "File does exist"
 else 
     git clone --progress https://github.com/eitea/T-Time.git /var/www/html/
+    echo "DirectoryIndex home.php" > /var/www/html/.htaccess
+    chown www-data:www-data  -R /var/www/html
+    find /var/www/html -type d -exec chmod 755 {} \;
+    find /var/www/html -type f -exec chmod 644 {} \;
 fi 
 /usr/local/bin/apache2-foreground

6c43e11d879924faf9f780a2deaa7f6286eeec9e
Lukas Troyer <36337818+lukas-tr@users.noreply.github.com>
2018-06-13 10:54:45 +0200

    use local directory instead of cloing from github

--- php/check_install.sh
+++ php/check_install.sh
@@ -1,14 +1,9 @@
-#!/bin/bash
-file=/var/www/html/home.php
+!/bin/bash
+file=/var/www/html/index.php
 if [ -e "$file" ]; then
     echo "File does exist"
     exec /usr/local/bin/docker-php-entrypoint
 else 
-    git clone --progress https://github.com/eitea/T-Time.git /var/www/html/
-    echo "DirectoryIndex home.php" > /var/www/html/.htaccess
-    chown www-data:www-data  -R /var/www/html
-    find /var/www/html -type d -exec chmod 755 {} \;
-    find /var/www/html -type f -exec chmod 644 {} \;
-    exec /usr/local/bin/docker-php-entrypoint
+    echo "File does not exist"
 fi 
 
